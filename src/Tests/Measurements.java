2d1a68d4da0b45ba64a1c3f65afd4a0c54e67d33
asafweinberg <74104951+asafweinberg@users.noreply.github.com>
2021-01-08 12:43:28 +0200

    test file

---
+++ src/Tests/Measurements.java
@@ -0,0 +1,20 @@
+package Tests;
+
+import Heap.FibonacciHeap;
+
+public class Measurements {
+
+	public static void main(String[] args) {
+		runSequence1();
+	}
+	
+	public static void runSequence1() {
+		FibonacciHeap heap;
+		sequence1(1024);
+	}
+	
+	public static void sequence1(int m) {
+		
+	}
+
+}
